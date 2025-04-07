2f01a551dc71c9b58d6f75f4f637ad6b4e85e651
davidcba01 <davoocordoba2001@gmail.com>
2025-04-07 19:13:06 -0300

    segundo commit

--- tp1_1.c
+++ tp1_1.c
@@ -4,5 +4,27 @@
 
 void main()
 {
-    printf("hola mundo");
+    printf("hola mundo\n\n");
+
+    // Declaro una variable de tipo int
+    int num = 42;
+
+    // Declaro un puntero que apunte a la variable 'num'
+    int *ptr = &num;
+
+    // Muestro el contenido del puntero (el valor al que apunta)
+    printf("1) Contenido del puntero: %d\n", *ptr);
+
+    // Muestro la dirección de memoria almacenada por el puntero
+    printf("2) Direccion de memoria almacenada por el puntero: %p\n", ptr);
+
+    // Muestro la dirección de memoria de la variable 'num'
+    printf("3) Direccion de memoria de la variable 'num': %p\n", &num);
+
+    // Muestro la dirección de memoria del puntero 'ptr'
+    printf("4) Direccion de memoria del puntero 'ptr': %p\n", &ptr);
+
+    // Muestro el tamaño de la variable 'num' usando sizeof()
+    printf("5) Tamaño de la variable 'num' en memoria: %zu bytes\n", sizeof(num));
+
 }
